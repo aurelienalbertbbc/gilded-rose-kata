2b81db32b2492d54f244fb1cbe5e46ca23634e68
Aurélien Albert <aurelien.albert@blablacar.com>
2024-05-02 10:25:00 +0200

    Sauvegarde 02/05/2024

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,49 @@
 # -*- coding: utf-8 -*-
+# Mais d'abord, laissez-moi vous présenter notre système :
+#
+# - Tous les éléments ont une valeur `sellIn` qui désigne le nombre de jours restant pour vendre l'article.
+# - Tous les articles ont une valeur `quality` qui dénote combien l'article est précieux.
+# - À la fin de chaque journée, notre système diminue ces deux valeurs pour chaque produit.
+#
+# Plutôt simple, non ?
+#
+# Attendez, ça devient intéressant :
+#
+# - Une fois que la date de péremption est passée, la qualité se dégrade deux fois plus rapidement.
+# - La qualité (`quality`) d'un produit ne peut jamais être négative.
+# - "Aged Brie" augmente sa qualité (`quality`) plus le temps passe.
+# - La qualité d'un produit n'est jamais de plus de 50.
+# - "Sulfuras", étant un objet légendaire, n'a pas de date de péremption et ne perd jamais en qualité (`quality`)
+# - "Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
+
+# Nous avons récemment signé un partenariat avec un fournisseur de produit invoqué ("Conjured"). Cela nécessite une mise à jour de notre système :
+#
+# les éléments "Conjured" voient leur qualité se dégrader de deux fois plus vite que les objets normaux.
+
+# to do
+# Type de produit en châine de caractères -> passer en Enum ?
+#
+
+
+
+# # At the end of the day: reduce sell date, update quality
+# sellDelta = 1
+# qualityDelta = 1
+#
+# if sellDelta < 0:
+# 	pass # When sell date is expired, its quality degrades 2 times faster
+#
+# # Some items have specific rules
+# if item.name = "Sulfuras":
+# 	sellDelta = 0
+# 	qualityDelta = 0
+#
+#
+# item.sellIn -= sellDelta
+# item.quality -= qualityDelta
+#
+# # Quality must be between 0 and 50
+# item.quality = max(0, min(item.quality, 50))
 
 class GildedRose(object):
 
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,15 +1,53 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+
 from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_generic_product_positive_quality(self):
+        Item("generic product", 0, -1)
+    def test_generic_product(self):
+        generic_product = Item("generic product", 10, 10)
+        items = [generic_product]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("generic product", items[0].name)
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(9, items[0].quality)
+
+    def test_generic_product_sell_in_passed(self):
+        generic_product = Item("generic product", 0, 10)
+        items = [generic_product]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("generic product", items[0].name)
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
+
+    def test_generic_product_no_negative_quality(self):
+        generic_product = Item("generic product", 10, 0)
+        items = [generic_product]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("generic product", items[0].name)
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
+
+    def test_aged_brie(self):
+        pass
+    
+    def test_backstage_passes(self):
+        pass
+
+    def test_sulfuras(self):
+        pass
+
+    def test_conjured(self):
+        pass
+
+
 
         
 if __name__ == '__main__':
