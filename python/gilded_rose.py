877bc93a81137b8e49755b1223942ca60409a399
Aurélien Albert <aurelien.albert@blablacar.com>
2024-05-15 22:14:06 +0200

    save

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,6 +15,8 @@
 # - La qualité d'un produit n'est jamais de plus de 50.
 # - "Sulfuras", étant un objet légendaire, n'a pas de date de péremption et ne perd jamais en qualité (`quality`)
 # - "Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ; La qualité augmente de 2 quand il reste 10 jours ou moins et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
+import enum
+
 
 # Nous avons récemment signé un partenariat avec un fournisseur de produit invoqué ("Conjured"). Cela nécessite une mise à jour de notre système :
 #
@@ -25,60 +27,81 @@
 #
 
 
-
 # # At the end of the day: reduce sell date, update quality
 # sellDelta = 1
 # qualityDelta = 1
-#
+# When sell date is expired, its quality degrades 2 times faster
 # if sellDelta < 0:
-# 	pass # When sell date is expired, its quality degrades 2 times faster
+# 	qualityDelta = 2
 #
 # # Some items have specific rules
-# if item.name = "Sulfuras":
-# 	sellDelta = 0
+# if item.name = "Sulfuras, Hand of Ragnaros":
+# 	sellDelta = 0      # It never expires. Not sure what that means in terms of rules
 # 	qualityDelta = 0
 #
+# # "Backstage passes", comme le "Aged Brie", augmente sa qualité (`quality`) plus le temps passe (`sellIn`) ;
+# # La qualité augmente de 2 quand il reste 10 jours ou moins
+# # et de 3 quand il reste 5 jours ou moins, mais la qualité tombe à 0 après le concert.
+# if item.name = "Backstage passes to a TAFKAL80ETC concert" or item.name == "Aged Brie":
+# 	if sellIn < 0:
+# 		qualityDelta = quality
+# 	if sellIn <= 5:
+# 		qualityDelta = -3 # negative because quality increases
+# 	if sellIn <= 10:
+# 		qualityDelta = -2
 #
 # item.sellIn -= sellDelta
 # item.quality -= qualityDelta
 #
 # # Quality must be between 0 and 50
 # item.quality = max(0, min(item.quality, 50))
+# # …except for Sulfuras
+# if item.name == "Sulfuras, Hand of Ragnaros":
+# 	item.quality = 80
+
+class SpecialProducts(enum.Enum):
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    CONJURED = "Conjured Mana Cake"
+
 
 class GildedRose(object):
 
     def __init__(self, items):
-        self.items = items
+        self.items = [ItemManager(item) for item in items]
 
     def update_quality(self):
+
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update_quality_and_sell_in()
+        #     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+        #         if item.quality > 0:
+        #             if item.name != "Sulfuras, Hand of Ragnaros":
+        #                 item.quality = item.quality - 1
+        #     else:
+        #         if item.quality < 50:
+        #             item.quality = item.quality + 1
+        #             if item.name == "Backstage passes to a TAFKAL80ETC concert":
+        #                 if item.sell_in < 11:
+        #                     if item.quality < 50:
+        #                         item.quality = item.quality + 1
+        #                 if item.sell_in < 6:
+        #                     if item.quality < 50:
+        #                         item.quality = item.quality + 1
+        #     if item.name != "Sulfuras, Hand of Ragnaros":
+        #         item.sell_in = item.sell_in - 1
+        #     if item.sell_in < 0:
+        #         if item.name != "Aged Brie":
+        #             if item.name != "Backstage passes to a TAFKAL80ETC concert":
+        #                 if item.quality > 0:
+        #                     if item.name != "Sulfuras, Hand of Ragnaros":
+        #                         item.quality = item.quality - 1
+        #             else:
+        #                 item.quality = item.quality - item.quality
+        #         else:
+        #             if item.quality < 50:
+        #                 item.quality = item.quality + 1
 
 
 class Item:
@@ -89,3 +112,21 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class ItemManager:
+    def __init__(self, item):
+        self.item = item
+
+    def update_quality_and_sell_in(self):
+        self.update_sell_in()
+        self.update_quality()
+
+    def update_quality(self):
+        quality_degradation = -1 if self.item.sell_in >= 0 else -2
+        self.item.quality = max(0, self.item.quality + quality_degradation)
+
+    def update_sell_in(self):
+        self.item.sell_in -= 1
+
+class Brie(ItemManager):
+    pass
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -2,15 +2,17 @@
 import unittest
 
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import GildedRose, Item
+from python.gilded_rose import SpecialProducts
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_generic_product_positive_quality(self):
-        Item("generic product", 0, -1)
+    def setUpSingleItem(self, name: str = "generic product", sell_in: int = 10, quality: int = 10):
+        generic_product = Item(name, sell_in, quality)
+        return [generic_product]
+
     def test_generic_product(self):
-        generic_product = Item("generic product", 10, 10)
-        items = [generic_product]
+        items = self.setUpSingleItem()
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual("generic product", items[0].name)
@@ -18,8 +20,7 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(9, items[0].quality)
 
     def test_generic_product_sell_in_passed(self):
-        generic_product = Item("generic product", 0, 10)
-        items = [generic_product]
+        items = self.setUpSingleItem(sell_in=0, quality=10)
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual("generic product", items[0].name)
@@ -27,22 +28,80 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(8, items[0].quality)
 
     def test_generic_product_no_negative_quality(self):
-        generic_product = Item("generic product", 10, 0)
-        items = [generic_product]
+        items = self.setUpSingleItem(sell_in=10, quality=0)
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual("generic product", items[0].name)
-        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(9, items[0].sell_in)
         self.assertEqual(0, items[0].quality)
 
     def test_aged_brie(self):
-        pass
-    
-    def test_backstage_passes(self):
-        pass
+        items = self.setUpSingleItem(name=SpecialProducts.AGED_BRIE.value, sell_in=10, quality=0)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(SpecialProducts.AGED_BRIE.value, items[0].name)
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
 
-    def test_sulfuras(self):
-        pass
+    def test_aged_brie_max_quality(self):
+        items = self.setUpSingleItem(name=SpecialProducts.AGED_BRIE.value, sell_in=10, quality=50)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(SpecialProducts.AGED_BRIE.value, items[0].name)
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(50, items[0].quality)
+
+    def test_backstage_more_than_10_days(self):
+        name = SpecialProducts.BACKSTAGE.value
+        items = self.setUpSingleItem(name=name, sell_in=15, quality=0)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(name, items[0].name)
+        self.assertEqual(14, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
+
+    def test_backstage_less_than_10_days(self):
+        items = self.setUpSingleItem(name=SpecialProducts.BACKSTAGE.value, sell_in=10, quality=0)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(SpecialProducts.BACKSTAGE.value, items[0].name)
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(2, items[0].quality)
+
+    def test_backstage_less_than_5_days(self):
+        items = self.setUpSingleItem(name=SpecialProducts.BACKSTAGE.value, sell_in=4, quality=0)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(SpecialProducts.BACKSTAGE.value, items[0].name)
+        self.assertEqual(3, items[0].sell_in)
+        self.assertEqual(3, items[0].quality)
+
+    def test_backstage_beyond_concert_date(self):
+        name = SpecialProducts.BACKSTAGE.value
+        items = self.setUpSingleItem(name=name, sell_in=0, quality=33)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(name, items[0].name)
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
+
+    def test_backstage_quality_below_50(self):
+        name = SpecialProducts.BACKSTAGE.value
+        items = self.setUpSingleItem(name=name, sell_in=4, quality=50)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(name, items[0].name)
+        self.assertEqual(3, items[0].sell_in)
+        self.assertEqual(50, items[0].quality)
+
+    def test_sulfuras_does_not_change(self):
+        name = SpecialProducts.SULFURAS.value
+        items = self.setUpSingleItem(name=name, sell_in=4, quality=80)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(name, items[0].name)
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
 
     def test_conjured(self):
         pass
